d20ca609dd6fc8217d74056a37600f95258754cc
mdmahmudurrahman <mailtommrr@gmail.com>
2020-09-17 17:03:52 +0200

    Test database connection with JDBC using a testservlet

--- src/com/luv2code/testdb/TestDbServlet.java
+++ src/com/luv2code/testdb/TestDbServlet.java
@@ -1,11 +1,15 @@
 package com.luv2code.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
 
 /**
  * Servlet implementation class TestDbServlet
@@ -19,7 +23,22 @@ public class TestDbServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=CET";
+		String user = "hbstudent";
+		String password = "hB@Student123";
+		String driver = "com.mysql.jdbc.Driver";
+		
+		try {
+			PrintWriter printWriter = response.getWriter();
+			printWriter.println("Connecting to database");
+			Class.forName(driver);
+			Connection myConnection = DriverManager.getConnection(jdbcUrl, user, password);
+			
+			printWriter.println("Success!!");
+			myConnection.close();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
 	}
 
 }
